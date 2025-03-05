c7789dc6b8535f4bd62d9087f8a17c781428d519
Kyeonghwan Ryu <kyeonghwan.ryu@moreh.io>
2025-03-05 01:51:49 +0000

    add gitignore

--- train_moreh.sh
+++ train_moreh.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-torchrun --nnodes=1 --node_rank=0 --nproc_per_node=1 --master_addr="0.0.0.0" --master_port="12234" ./train_moreh.py \
+torchrun --nnodes=1 --node_rank=0 --nproc_per_node=8 --master_addr="0.0.0.0" --master_port="12234" ./train_moreh.py \
     --config_file configs/llama-3.1-70b-moreh.json \
     --model_path /vast/huggingface/saved/regisss--llama2-70b-fused-qkv-mlperf.pt \
     --dataset_path /vast/huggingface/hub/datasets--regisss--scrolls_gov_report_preprocessed_mlperf_2/snapshots/21ff1233ee3e87bc780ab719c755170148aba1cb/data/train-00000-of-00001.parquet \
